69fa6ef237511e17d0dcd0d502e6fb27a0d08707
Patrick Haas <patmanteau@posteo.de>
2018-03-14 12:13:24 +0100

    Rename extension.ts

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,37 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+//import * as axios from 'axios';
+const axios = require('axios');
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "zotero-citations-vsc" is now active!');
+    
+    // console.log('Congratulations, your extension "zotero-citations-vsc" is now active!');
+
+    let pickCitation = async () => {
+        const configuration = vscode.workspace.getConfiguration('zotero-citations-vsc');
+        const format = configuration.get("format", "pandoc");
+        const url = `http://localhost:23119/better-bibtex/cayw?format=${format}`;
+        const response = await axios.get(url);
+        
+        let editor = vscode.window.activeTextEditor;
+        if (editor) {
+            let selection = editor.selection;
+            editor.edit( (editBuilder) => {
+                editBuilder.insert(selection.active, response.data);
+            });
+        }
+    };
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    let disposable = vscode.commands.registerCommand('zotero-citations-vsc.pickCitation', pickCitation);
 
     context.subscriptions.push(disposable);
 }
